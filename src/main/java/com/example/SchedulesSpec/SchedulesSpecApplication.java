72f5c67d9e940ff7f349bd919a8cf6bf788acaba
Ruslan7173 <lifecol57@gmail.com>
2024-11-28 00:32:52 +0300

    картинка

--- src/main/java/com/example/SchedulesSpec/SchedulesSpecApplication.java
+++ src/main/java/com/example/SchedulesSpec/SchedulesSpecApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchedulesSpecApplication {
+public class
+SchedulesSpecApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulesSpecApplication.class, args);
--- src/main/java/com/example/SchedulesSpec/Specialist.java
+++ src/main/java/com/example/SchedulesSpec/Specialist.java
@@ -5,6 +5,14 @@ public class Specialist {
     private Long id;
     private String name;
     private String name1;
+    private String name2;
+
+    public Specialist(Long id, String name, String name1, String name2) {
+        this.id = id;
+        this.name = name;
+        this.name1 = name1;
+        this.name2 = name2;
+    }
 
     public Specialist(String name1, String name, Long id) {
         this.name1 = name1;
@@ -16,6 +24,10 @@ public class Specialist {
     public Specialist(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public String getName2() {
+        return name2;
     }
 
     public String getName1() {
